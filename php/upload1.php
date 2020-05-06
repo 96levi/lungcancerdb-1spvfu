f418f9f3f0b87afc7cf8717832ba9bb0149b9a8e
LeVi <levi96.res@gmail.com>
2020-05-06 12:33:20 +0700

    Add files via upload

---
+++ php/upload1.php
@@ -0,0 +1,42 @@
+<?php
+// Remote from NG app (on Personal Computer)
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: POST');
+
+$target_dir = "./img/".$_GET['folder']."/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["image"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+        print json_encode("The file " . basename($_FILES["image"]["name"]) . " has been uploaded to folder: ".$target_dir);
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
